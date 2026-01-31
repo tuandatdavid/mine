c6be6710d90230110ec0f853658992f4f8b8d084
Tuan Dat Pham <88708244+tuandatdavid@users.noreply.github.com>
2026-01-31 22:12:14 +0100

    i am not sure anymore

--- files/scripts/signkernel.sh
+++ files/scripts/signkernel.sh
@@ -12,7 +12,8 @@ KERNEL_VERSION="$(rpm -q "kernel-cachyos-lto" --queryformat '%{VERSION}-%{RELEAS
 
 PUBLIC_KEY_DER_PATH="/etc/pki/akmods/certs/akmods-blue-build.der"
 PUBLIC_KEY_CRT_PATH="./public_key.crt"
-PRIVATE_KEY_PATH="/tmp/certs/private_key.priv"
+PRIVATE_KEY_PATH="./private_key.priv"
+cp /home/runner/work/mine/mine/private_key.priv ./private_key.priv
 openssl x509 -in "$PUBLIC_KEY_DER_PATH" -out "$PUBLIC_KEY_CRT_PATH"
 sbsign --cert "$PUBLIC_KEY_CRT_PATH" --key "$PRIVATE_KEY_PATH" /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz --output /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
 sbverify --list /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
