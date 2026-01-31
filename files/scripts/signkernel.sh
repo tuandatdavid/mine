e4df05fa7f9d679ad8d608199517003a84288843
Tuan Dat Pham <88708244+tuandatdavid@users.noreply.github.com>
2026-01-31 21:14:11 +0100

    wrong path!!!

--- files/scripts/signkernel.sh
+++ files/scripts/signkernel.sh
@@ -9,10 +9,9 @@ set -euo pipefail
 
 KERNEL_VERSION="$(uname -r)"
 
-PUBLIC_KEY_DER_PATH="./system/etc/pki/akmods/certs/akmods-blue-build.der"
+PUBLIC_KEY_DER_PATH="./files/system/etc/pki/akmods/certs/akmods-blue-build.der"
 PUBLIC_KEY_CRT_PATH="/tmp/certs/public_key.crt"
 PRIVATE_KEY_PATH="/tmp/certs/private_key.priv"
-mkdir /tmp/certs && touch $PUBLIC_KEY_CRT_PATH
 openssl x509 -in "$PUBLIC_KEY_DER_PATH" -out "$PUBLIC_KEY_CRT_PATH"
 sbsign --cert "$PUBLIC_KEY_CRT_PATH" --key "$PRIVATE_KEY_PATH" /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz --output /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
 sbverify --list /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
