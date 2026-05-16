8795aa8f7ced45b722d5dc8610fae4d5f9db0e85
Tuan Dat Pham <88708244+tuandatdavid@users.noreply.github.com>
2026-05-16 12:13:33 +0200

    install sbsigntools IN THE SCRIPT

--- files/scripts/signkernel.sh
+++ files/scripts/signkernel.sh
@@ -13,6 +13,7 @@ KERNEL_VERSION="$(rpm -q "kernel-cachyos-lto" --queryformat '%{VERSION}-%{RELEAS
 PUBLIC_KEY_DER_PATH="/etc/pki/akmods/certs/akmods-blue-build.der"
 PUBLIC_KEY_CRT_PATH="./public_key.crt"
 PRIVATE_KEY_PATH="/tmp/certs/private_key.priv"
+dnf5 -y install sbsigntools
 openssl x509 -in "$PUBLIC_KEY_DER_PATH" -out "$PUBLIC_KEY_CRT_PATH"
 sbsign --cert "$PUBLIC_KEY_CRT_PATH" --key "$PRIVATE_KEY_PATH" /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz --output /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
 sbverify --list /usr/lib/modules/"${KERNEL_VERSION}"/vmlinuz
